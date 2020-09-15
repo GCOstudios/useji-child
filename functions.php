f4bb7f7e7b29881038f895cb7610821ec80901dc
Jose Guerra <jose@gcostudios.com>
2020-09-15 09:19:24 +0100

    added version number

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function mts_child_enqueue_scripts() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), '20201509');
 }
 add_action( 'wp_enqueue_scripts', 'mts_child_enqueue_scripts' );
 
