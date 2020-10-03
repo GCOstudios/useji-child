ebfdf9600183e22db0fab24bd66187023a8f91ee
Jose Guerra <jose@gcostudios.com>
2020-10-03 17:21:27 +0100

    updated markup

--- page-custom.php
+++ page-custom.php
@@ -7,11 +7,13 @@
 	<article class="<?php mts_article_class(); ?>">
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
         <header>
-          <h1 class="title entry-title"><?php the_title(); ?></h1>
+          <div class="container">
+            <h1 class="title entry-title"><?php the_title(); ?></h1>
+          </div>
         </header>
         <div class="entry-content">
           <?php the_content(); ?>
-        </div><!--.post-content box mark-links-->
+        </div><!--.post-content-->
 			<?php endwhile; ?>
 	</article>
 <?php get_footer(); ?>
