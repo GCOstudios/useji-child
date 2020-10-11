49bcd3ce6d9495807d9e1ec170911a08998909d3
Jose Guerra <jose@gcostudios.com>
2020-10-11 21:51:27 +0100

    added custom cart

--- functions.php
+++ functions.php
@@ -12,6 +12,8 @@ function mts_child_enqueue_scripts() {
       array( $parenthandle ),
       $theme->get('Version')
   );
+
+  wp_enqueue_script('child-js', get_stylesheet_directory_uri() . '/js/custom.min.js', array(), '1.0.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'mts_child_enqueue_scripts' );
 
@@ -22,3 +24,20 @@ register_nav_menus( array(
 
 // remove the clear variation link
 add_filter('woocommerce_reset_variations_link', '__return_empty_string');
+
+/**
+ * Show cart contents / total Ajax
+ */
+add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
+
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+	?>
+	<a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> – <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+	<?php
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	return $fragments;
+}
