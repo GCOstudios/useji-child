0c25fbbd3152f451de218e8a3d74ed103e3ecbdd
Jose Guerra <jose@gcostudios.com>
2020-10-11 21:52:20 +0100

    added test script

---
+++ src/js/custom.js
@@ -0,0 +1,8 @@
+// const wooTabs = document.getElementsByTagName('a');
+
+// for (let i = 0; i < wooTabs.length; i++) {
+//   wooTabs[i].addEventListener('click', function(e) {
+//     return false;
+//     console.log('click works');
+//   });
+// }
