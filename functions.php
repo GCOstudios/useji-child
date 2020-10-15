0538d67cde494dd4dcfc2d2bfb553ee6d968b38c
Jose Guerra <jose@gcostudios.com>
2020-10-15 12:45:24 +0100

    updated functions

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ function mts_child_enqueue_scripts() {
       $theme->get('Version')
   );
 
-  wp_enqueue_script('child-js', get_stylesheet_directory_uri() . '/js/custom.min.js', array(), '1.0.0', true);
+  wp_enqueue_script('child-js', get_stylesheet_directory_uri() . '/js/custom.min.js', array('jquery'), '1.0.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'mts_child_enqueue_scripts' );
 
@@ -25,19 +25,32 @@ register_nav_menus( array(
 // remove the clear variation link
 add_filter('woocommerce_reset_variations_link', '__return_empty_string');
 
-/**
- * Show cart contents / total Ajax
- */
-add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
 
-function woocommerce_header_add_to_cart_fragment( $fragments ) {
-	global $woocommerce;
-
-	ob_start();
-
-	?>
-	<a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> – <?php echo $woocommerce->cart->get_cart_total(); ?></a>
-	<?php
-	$fragments['a.cart-customlocation'] = ob_get_clean();
-	return $fragments;
+//* Dequeue scripts and styles
+add_action( 'wp_enqueue_scripts', 'bn_disable_woocommerce_loading_css_js' );
+ 
+function bn_disable_woocommerce_loading_css_js() {
+ 
+  // Check if WooCommerce plugin is active
+  if( function_exists( 'is_woocommerce' ) ){
+  
+    // Check if it's any of WooCommerce page
+    if(! is_woocommerce() && ! is_cart() && ! is_checkout() ) {         
+    
+        ## Dequeue WooCommerce styles
+        wp_dequeue_style('woocommerce-layout'); 
+        wp_dequeue_style('woocommerce-general'); 
+        wp_dequeue_style('woocommerce-smallscreen');
+        wp_dequeue_style('wc-block-style');
+    
+        ## Dequeue WooCommerce scripts
+        wp_dequeue_script('wc-cart-fragments');
+        wp_dequeue_script('woocommerce'); 
+        wp_dequeue_script('wc-add-to-cart'); 
+        wp_deregister_script( 'js-cookie' );
+        wp_dequeue_script( 'js-cookie' );
+        wp_dequeue_script( 'vc_woocommerce-add-to-cart-js' );
+    
+    }
+  }   
 }
