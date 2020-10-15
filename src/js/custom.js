da49e250de09576cfa559587f86e08f19fbdecfb
Jose Guerra <jose@gcostudios.com>
2020-10-15 12:46:05 +0100

    updated test scripts

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,34 @@
-// const wooTabs = document.getElementsByTagName('a');
+// jQuery(document).ready(function($) {
 
-// for (let i = 0; i < wooTabs.length; i++) {
-//   wooTabs[i].addEventListener('click', function(e) {
-//     return false;
-//     console.log('click works');
+//   // Prevent default on anchor link click
+//   $("a[href^='#']").click(function(e) {
+//     e.preventDefault();
+//     changeHashWithoutScrolling($(this).attr('href'));
 //   });
-// }
+
+//   // Prevent instant scroll of browser
+//   function changeHashWithoutScrolling(hash){
+//     if($(hash).length > 0){
+//         // var $el = $(hash);
+//         // var elId = $el.attr('id');
+//         // $el.removeAttr('id');
+
+//         window.location.hash = hash;
+
+//         // $el.attr('id', elId);
+//     }
+//   }
+// });
+
+const prodTabs = document.querySelectorAll('a[href^="#"]');
+
+for (let i = 0; i < prodTabs.length; i++) {
+  prodTabs[i].addEventListener('click', function(e) {
+    e.preventDefault();
+
+    let newHash = window.location.hash;
+
+    // window.location.hash = hash;
+    console.log(this)
+  });
+}
